95f49e8f94a5a9f2b50e074effed75e0f468db71
Gengchao Yang <yanggengchao@outlook.com>
2026-01-17 01:24:29 +1030

    Update default title in route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,8 +1,9 @@
+// app/og/route.tsx
 import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
   let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+  let title = url.searchParams.get('title') || 'Gengchao Yang Portfolio'
 
   return new ImageResponse(
     (
